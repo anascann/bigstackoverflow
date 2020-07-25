10dc8277b894901e328289c80bf7f7e8aba73d04
Anas khan <lieuanas456@gmail.com>
2020-07-25 22:10:25 +0530

    register route

---
+++ models/Person.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const PersonSchema=new Schema({
+    name:{
+        type:String,
+        required: true
+    },
+
+    email:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    profilepic:{
+        type:String,
+        default:'https://pixabay.com/photos/ballet-dancer-oudoors-elegant-5415806/'
+    },
+
+    date:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports=Person=mongoose.model("myPerson",PersonSchema);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,45 @@
 const express=require('express');
+const bcrypt=require('bcryptjs');
+const jsonwt=require('jsonwebtoken');
+const passport=require('passport');
+const key=require('../../setup/mongourl');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 const router=express.Router();
 
 router.get('/',(req,res)=> res.json({test:'Auth is active'}));
+
+const Person=require('../../models/Person');
+
+router.post('/register',(req,res)=>{
+    Person.findOne({email: req.body.email})
+        .then(person=>{
+            if(person){
+                return res.status(400).json({emailerror:'email is already registered'})
+            }else{
+                const newPerson=new Person({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(saltRounds, (err, salt) =>{
+                    bcrypt.hash(newPerson.password, salt, (err, hash) =>{
+                        // Store hash in your password DB.
+                        if(err)throw err;
+                        newPerson.password=hash;
+                        newPerson
+                            .save()
+                            .then(person=>res.json(person))
+                            .catch(err=>console.log(err))
+                    });
+                });
+            }
+        })
+        .catch(err=>console.log(err));
+})
+
+
 
 module.exports=router;
